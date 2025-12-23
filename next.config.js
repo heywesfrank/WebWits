e017ab4ebd6460f508700021d509c82d8e4084c9
heywesfrank <142755253+heywesfrank@users.noreply.github.com>
2025-12-23 12:11:30 -0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['你的supabase-project-id.supabase.co', 'images.unsplash.com'],
+  },
+};
+
+module.exports = nextConfig;
